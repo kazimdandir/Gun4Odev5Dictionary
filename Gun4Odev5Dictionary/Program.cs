036f64fca319564279a1a3db868bc519f94d8ef1
kazimdandir <kikbal.dandir@gmail.com>
2023-03-23 15:05:24 +0300

    Add project files.

---
+++ Gun4Odev5Dictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gun4Odev5Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
